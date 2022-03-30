4bbc2f53352717cc16fa9cd6fdfbcf78c7556508
Manav Naharwal <manavnaharwalp@gmail.com>
2022-03-30 13:47:17 +0530

    Fix the scroll-behaviour problem when navigating to previous page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
